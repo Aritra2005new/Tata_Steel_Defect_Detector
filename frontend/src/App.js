f11206e4eec7d879cf0f9fe27889174d6c2a9dca
Aritra Nandi <n.aritranandi789@gmail.com>
2026-05-20 23:06:25 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -162,7 +162,12 @@ export default function App() {
           const fd = new FormData();
           fd.append("image", item.file);
           try {
-            return await axios.post(API_URL, fd);
+            return await axios.post(API_URL, fd, {
+            headers: {
+            "Content-Type": "multipart/form-data",
+            },
+            timeout: 120000,
+            });
           } catch {
             return null;
           }
